a0780d870574d34f1ed080dd57ebd9469b282b67
Alvan Mojos <your_github_email@example.com>
2026-04-07 22:51:14 +0800

    update setup

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,29 +1,28 @@
-import { test as setup, expect } from '@playwright/test'
 import fs from 'fs';
 import path from 'path';
+import { test as setup, expect } from '@playwright/test';
 
 const authFile = path.join(process.cwd(), 'playwright', '.auth', 'user.json');
 
-setup('authecticated page setup', async ({ page, request }) => {
+setup('authenticated page setup', async ({ page, request }) => {
   fs.mkdirSync(path.dirname(authFile), { recursive: true });
 
-   const apiContext = await request.post(`${process.env.API_URL}/auth/login`,{
-      data: {
-        userEmail: process.env.USER_EMAIL,
-        userPassword: process.env.USER_PASSWORD,
-      },
-    });
+  const loginResponse = await request.post(`${process.env.API_URL}/auth/login`, {
+    data: {
+      userEmail: process.env.USER_EMAIL,
+      userPassword: process.env.USER_PASSWORD,
+    },
+  });
 
-    expect((apiContext).ok()).toBeTruthy();
+  expect(loginResponse.ok()).toBeTruthy();
 
-    const body = await apiContext.json();
+  const body = await loginResponse.json();
+  const token = body.token;
 
-    const token = body.token;
+  await page.goto('/');
+  await page.evaluate((value: string) => {
+    localStorage.setItem('token', value);
+  }, token);
 
-    await page.goto('/');
-    await page.evaluate((value: string) => {
-      localStorage.setItem('token', value)
-    },token);
-
-    await page.context().storageState({ path: authFile });
-})
+  await page.context().storageState({ path: authFile });
+});
